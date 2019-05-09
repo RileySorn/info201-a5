380c22489adf0c60edcbe59a68f48ed8361ff6d9
ryrytio <hstprime@gmail.com>
2019-05-09 00:25:40 -0700

    finally done
    yessss

--- analysis.R
+++ analysis.R
@@ -25,7 +25,6 @@ max_injured <- max(shootings_2018$num_injured) #19 injured
 
 # summary table
 
-
 four_states <- select(shootings_2018, state, num_killed, num_injured) %>%
 filter(state == "Illinois" | state == "California" | state == "New York"
  | state == "Michigan")
@@ -56,36 +55,43 @@ ny_summary <- four_states %>%
             total_injured = sum(num_injured))
 
 four_st_summ <- rbind(cali_summary, illi_summary, mich_summary, ny_summary)
+#combines each state's information.
 kable(four_st_summ, col.names =  c("state", "total_killed",
                                   "total_injured")) #yeet
 
-#Particular Incident 
+#Particular Incident
+#  date of incident
 inc_date <- shootings_2018 %>%
   select(date) %>%
   filter(date == "May 1, 2018") %>%
 pull(date)
 
+#  city where incident occurred
 inc_city <- shootings_2018 %>%
   group_by(city) %>%
   filter(date == "May 1, 2018") %>%
 pull(city)
 
+#  state where incident ocurred
 inc_state <- shootings_2018 %>%
   group_by(state) %>%
   filter(date == "May 1, 2018") %>%
 pull(state)
 
+#  number of people killed in incident
 inc_killed <- shootings_2018 %>%
   group_by(num_killed) %>%
   filter(date == "May 1, 2018") %>%
   pull(num_killed)
-
+#  number of people injured
 inc_injured <- shootings_2018 %>%
   group_by(num_injured) %>%
   filter(date == "May 1, 2018") %>%
   pull(num_injured)
 
 #interactive plot
+#  making a map that contains coordinates of each shooting and then containing
+#   information of city, state, number of injured, and number of killed
 shootings_map <- leaflet(shootings_2018) %>%
   addTiles() %>%
   addMarkers(
@@ -94,6 +100,8 @@ shootings_map <- leaflet(shootings_2018) %>%
   )
 
 # My own plot
+#  I made a bar graph that displays four major states that are geographically
+#   within a lot of shootings
 condensed_states <- shootings_2018 %>%
   group_by(state) %>%
   filter(state == "Illinois" | state == "New York" | state == "Florida" |
